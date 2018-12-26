7461054599c74c4c1809bb704735ef83a6519782
Jeff A. Korthuis <jak@saturna.com>
2018-12-26 14:20:09 -0800

    Cleaned up the old script

--- PyAsset.py
+++ PyAsset.py
@@ -1,38 +1,125 @@
 #!/usr/bin/env python
 #
 #	PyAsset.py
-#	Your one-stop shop for all asset stuff
-# python PyAsset.py --MAC "78:31:c1:c1:c1:42" --function "get" --serial "C02M123GFH05" --model "MACBookPro11,1" --category "Laptops" --notes "testing testing 123" --status "Deployed" --DNS_name "jak-2131" --MAC_Eth0 "ac:87:a3:14:ce:03" --MAC_WiFi "78:31:c1:c1:76:42" --Munki_Manifest "satjak" --munki_roles "testing"
 
 __author__ = 'Jeff Korthuis (jeffkorthuis@gmail.com)'
-__version___ = '0.3.0'
+__version___ = '0.4.0'
 
 import json
 import requests
 import argparse
 import logging
+global myToken
+global snipeURL
+global jsonHeader
+global defaultResults
+defaultResults = ['id','asset_tag','name','serial','model_number','munki_roles','assigned_to','status_label','location']
+snipeURL = 'https://assets.something.com/api/v1/'
+myToken = 'Bearer token'
+jsonHeader = 'application/json'
 
+class API:
+	#This is the main function. It assumes you know 1) the table you want + 2) the ID of the object you want
+	def __init__(self,Table,ID):
+		self.Table = Table
+		self.ID = ID
+		self.URL = snipeURL + str(Table) + "/" + str(ID)
+		self.r = requests.get(self.URL,headers={'Authorization': myToken,'Content-Type':'application/json'})
+	def __call__(self):
+		return self.r.json()
+
+class SEARCH:
+	#This is the function that find the object you are looking for. Pass it any string, and any table and it will pass back the results of the search. This will always exit the script unless it returns only 1 result
+	def __init__(self,Table,SearchString):
+		self.Table = Table
+		self.SearchString = SearchString
+		self.URL = snipeURL + str(Table) + "?search=" + str(SearchString)
+		self.Results = requests.get(self.URL,headers={'Authorization': myToken,'Content-Type':'application/json'})
+	def __call__(self):
+		###Searches can result in multipul values. If we only get 1 result back it is a succesfful search. Everything else will exit
+		resultCount = self.Results.json()['total']
+		if resultCount == 1:
+			return self.Results.json()['rows'][0]
+		else:
+			results = self.Results.json()['rows']
+			self.multipulResults(results,resultCount)
+
+	def multipulResults(self,results,resultCount):
+		#Multi Response Error Handling#
+		if resultCount == 0:
+			print "No results found for " + str(self.Table) + "search: " + str(self.SearchString)
+			exit()
+		else:
+			print "Multi-Results found on search: " + self.URL
+			for item in results:
+				results = RETURN(item)
+				print results()
+			exit()
+
+class CHECKIN:
+	def __init__(self,hardwareInfo):
+		self.hardwareInfo = hardwareInfo
+		postData = {}
+		postData['name'] = hardwareInfo['name']
+		postData['note'] = userVariables.notes
+		postFields = json.dumps(postData, sort_keys=True,indent=2)
+		URL = snipeURL + 'hardware/' + str(hardwareInfo['id']) + "/checkin"
+		r = requests.post(URL,headers={'Authorization': myToken,'Content-Type': jsonHeader},data=postFields)
+		print r.json()['messages']
+
+class CHECKOUT:
+	def __init__(self,hardwareInfo,userInfo):
+		self.hardwareInfo = hardwareInfo
+		self.userInfo = userInfo
+		postData = {}
+		postData['name'] = self.hardwareInfo['name']
+		postData['note'] = userVariables.notes
+		postData['assigned_user'] = userInfo['id']
+		postData['checkout_to_type'] = 'user'
+		postFields = json.dumps(postData, sort_keys=True,indent=2)
+		self.URL = snipeURL + 'hardware/' + str(hardwareInfo['id']) + "/checkout"
+		if self.hardwareInfo['assigned_to']:
+			checkinCMD = CHECKIN(hardwareInfo)
+		r = requests.post(self.URL,headers={'Authorization': myToken,'Content-Type':'application/json'},data=postFields)
+		itemInfo = RETURN(hardwareInfo)
+		print itemInfo()
+		print r.json()['messages']
+
+class RETURN:
+	def __init__(self, hardwareInfo):
+		self.hardwareInfo = hardwareInfo
+		self.results = {}
+		for item in self.hardwareInfo:
+			if item in defaultResults:
+				if type(self.hardwareInfo[item]) == dict:
+					self.results[item] = self.hardwareInfo[item]['name']
+				else:
+					self.results[item] = self.hardwareInfo[item]
+		for item in self.hardwareInfo['custom_fields']:
+			if item in defaultResults:
+				self.results[item] = self.hardwareInfo['custom_fields'][item]['value']
+	def __call__(self):
+		return json.dumps(self.results, indent=2)
+
+#####
+#MAIN
+#
 class MainProcess():
 	def iRUNFIRST(self):
-		global myToken
+
 		global userVariables
-		global snipeURL
-		global jsonHeader
-		#########SYSTEM VARIABLES#########
-		snipeURL = 'https://assets.something.com/api/v1/'
-		defaultResults = ['id','asset_tag','name','serial','model_number','Mac_Address_(Ethernet)','Mac_Address_(WiFi)','Munki_Manifest','munki_roles','assigned_to','category','status_label','location']
-		myToken = 'Bearer someapikey'
-		jsonHeader = 'application/json'
 		########################setup the logger####################
 		self.loggingSetup()
 		logger.info("----PYASSETS BEGIN----")
-		userVariables = self.getVariables(defaultResults)
+		userVariables = self.getVariables()
+
 		logger.info("Arguements passed: " + str(userVariables))
 
-		####################main process#####################
-		####GET####
 		if userVariables.function == 'get':
-			self.getRun()
+			searchRequest = SEARCH('hardware',userVariables.hardware)
+			hardwareInfo = API('hardware',searchRequest()['id'])
+			results = RETURN(hardwareInfo())
+			print results()
 
 		####CREATE####
 		elif userVariables.function == 'create':
@@ -41,11 +128,17 @@ class MainProcess():
 
 		####CHECKOUT####
 		elif userVariables.function == 'checkout':
-			self.checkoutRun()
+			searchRequest = SEARCH('hardware',userVariables.hardware)
+			searchUser = SEARCH('users',userVariables.user)
+			hardwareInfo = API('hardware',searchRequest()['id'])
+			checkoutCMD = CHECKOUT(hardwareInfo(),searchUser())
+
 
 		####CHECKIN####
 		elif userVariables.function == 'checkin':
-			self.checkinRun()
+			searchRequest = SEARCH('hardware',userVariables.hardware)
+			hardwareInfo = API('hardware',searchRequest()['id'])
+			checkinCMD = CHECKIN(hardwareInfo())
 
 		####FAIL####
 		else:
@@ -53,209 +146,32 @@ class MainProcess():
 			logger.info("----END RUN----")
 			exit()
 
-	def output(self,hardwareInfo):
-		##########
-		# inputs: hardwareInfo
-		#
-		#
-		# Output: prints the information requested from
-		# --query
-		# --hardware
-		allFields = {}
-		global CustomFields
-		CustomFields = self.getCustomFields()
-		logger.info("Custom Fields Present in DB: " + str(CustomFields))
-		for x in userVariables.query:
-			try:
-				if str(x) in CustomFields:  #If the value you are looking for is a custom field
-					allFields[str(x)] = str(self.getCustomField(x,hardwareInfo))
-				elif type(hardwareInfo[str(x)]) == dict: #elif it has a dictionary inside
-					try:
-						allFields[str(x)] = hardwareInfo[str(x)]['name']
-					except:
-						allFields[str(x)] = hardwareInfo[str(x)]
-				else: #else it is just a plain field
-					if hardwareInfo[str(x)]:
-							allFields[str(x)] = str(hardwareInfo[str(x)])
-					else: #maybe it is null
-						allFields[str(x)] = ''
-			except: #NONE OF THE ABOVE
-				logger.info("The following field was non-existant or null: " + str(x))
-
-		print json.dumps(allFields,sort_keys=True, indent=2)
-		logger.info(json.dumps(allFields, indent=2))
-		logger.info("----PYASSETS EXIT SUCCESSFUL----")
-
-	def getRun(self):
-		##########
-		# inputs: None
-		#
-		#
-		# Output: hardwareInfo
-		#
-		#
-		logger.info("Function: 'getRun'" + " // " + str(userVariables.hardware))
-		AssetID = self.getField('hardware',userVariables.hardware,'id')
-		hardwareInfo = self.getInfo(AssetID,'hardware')
-		logger.info("Finalizing output: // " + "AssetID: " + str(AssetID) + " // Asset_Tag: " + str(hardwareInfo['asset_tag']))
-		getReturn = json.dumps(hardwareInfo, sort_keys=True,indent=2)
-		self.output(hardwareInfo)
-
-	def checkinRun(self):
-		##########
-		# inputs: None
-		#
-		#
-		# Output: print of the results of a checkin
-		#
-		#
-		logger.info("starting 'checkinRun'")
-		AssetID = self.getField('hardware',userVariables.hardware,'id')
-		logger.info("AssetID: " + str(AssetID))
-		postData = {}
-		postData['name'] = self.getInfo(AssetID,'hardware')['name']
-		postData['note'] = userVariables.notes
-		postFields = json.dumps(postData, sort_keys=True,indent=2)
-		postURL = snipeURL + 'hardware/' + str(AssetID) + "/checkin"
-		logger.info("Performing a checkin:  " + postFields)
-		r = requests.post(postURL,headers={'Authorization': myToken,'Content-Type': jsonHeader},data=postFields)
-		print r.json()['messages'] + " // "  + postData['name']
-
-	def checkoutRun(self):
-		##########
-		# inputs: None
-		#
-		#
-		# Output: print of the results of a checkout
-		#
-		#
-		logger.info("Function: 'checkoutRun'")
-		if not userVariables.user:
-			print "please provide a username -u --user \"jak\""
-			exit()
-		AssetID = self.getField('hardware',userVariables.hardware,'id')
-		UserID = self.getField('users',userVariables.user,'id')
-		postData = {}
-		hardwareInfo = self.getInfo(AssetID,'hardware')
-		######################Construct a response#################
-		postData['name'] = hardwareInfo['name']
-		postData['note'] = userVariables.notes
-		postData['assigned_user'] = UserID
-		postData['checkout_to_type'] = 'user'
-		postFields = json.dumps(postData, sort_keys=True,indent=2)
-		##########################################################
-		logger.info("Performing a checkout to user: " + str(UserID) + " // " + str(postFields))
-		postURL = snipeURL + 'hardware/' + str(AssetID) + "/checkout"
-		logger.info ("POST URL: " + str(postURL))
-		if userVariables.testing == 'true':
-			logger.info("THIS WAS A TEST RUN")
-			print "Test Run"
-		else:
-			r = requests.post(postURL,headers={'Authorization': myToken,'Content-Type':'application/json'},data=postFields)
-			print r.json()['messages'] + " // "  + str(hardwareInfo['name']) + "  //  " + str(self.getInfo(UserID,'users')['name'])
-			if r.json()['messages'] == 'That asset is not available for checkout!':
-				logger.info("----RUNNING CHECKIN-----")
-				self.checkinRun()
-				self.checkoutRun()
-
-	def getField (self,Table,Lookup,LookupObject):
-		##########
-		# inputs:
-		# Table = the api/v1/TABLE/
-		# Lookup = the value that we are looking up. For instance with api/v1/users/ we would look up jak
-		# Lookup Object: The field value you wish to get returned
-		#
-		# Example call: self.getField('hardware','some device','id') // this would return the ID of 'some device'
-		logger.info("Function: 'getField'" + " // " +str(Table) + " // "+ str(Lookup) + " // "+ str(LookupObject) + " // ")
-		searchURL = snipeURL + str(Table) + "?search=" + str(Lookup)
-		r = requests.get(searchURL,headers={'Authorization': myToken,'Content-Type':'application/json'})
-		foundItems = r.json()['total']
-
-		#Multi Response Error Handling#
-		if foundItems == 1:
-			jsonResponse = r.json()['rows'][0]
-			return jsonResponse[LookupObject]
-		else:
-			jsonResponse = r.json()['rows']
-			print "ASSET TAG    //    ASSET NAME    //     ASSIGNED TO"
-			print "--------------------------------------------------"
-			for item in r.json()['rows']:
-				try:
-					print item['asset_tag'] + "    //    " + item['name'] + "    //   " + str(item['assigned_to']['name'])
-					logger.info(item['asset_tag'] + "    //    " + item['name'] + "    //   " + str(item['assigned_to']['name']))
-				except:
-					print item['asset_tag'] +    "    //    " + item['name'] + "    //    None "
-					logger.info(item['asset_tag'] + "    //    " + item['name']+ "    //    None "  )
-			print "--------------------------------------------------"
-			logger.info("----SYSTEM EXITED----")
-			exit()
-
-	def getInfo(self, ID,Table):
-		##########
-		# inputs:
-		# ID: The ID value of a known api lookup location must be proided to this funtion
-		# Table: the know table that we are looking up.
-		# Output: a json blob of the output
-		# Example call: self.getField('hardware','jak-mbp-2131','id') // this would return the ID of jak-mbp-2131
-		logger.info("Function: gethardwareInfo " + " // " + str(ID) + " // " + str(Table))
-		searchURL = snipeURL + Table + "/"+ str(ID)
-		r = requests.get(searchURL,headers={'Authorization': myToken,'Content-Type':'application/json'})
-		hardwareInfo = r.json()
-		return hardwareInfo
-
-	def testingRun(self, ID,Table):
-		logger.info("Function: 'testingRun'")
-		print "<-----TESTING----->"
-		print "<-----TESTING----->"
-
-	def getCustomFields(self):
-		##########
-		# inputs: None
-		#
-		# Output: a table with all the custom fields names in it
-		custom_Fields = []
-		searchURL = snipeURL + 'fields/'
-		r = requests.get(searchURL,headers={'Authorization': myToken,'Content-Type':'application/json'})
-		for field in r.json()['rows']:
-			custom_Fields.append(str(field['name']))
-		logger.info("Function: 'getCustomField" + " // " + str(custom_Fields))
-		return custom_Fields
-
-	def getCustomField(self,Field,HardwareInfo):
-		##########
-		# inputs:
-		# Field - Custom Field Name
-		# HardwareInfo - the JSON array from the function getField
-		#
-		# Output: the value of the FIELD provided
-		#
-		logger.info("Function: 'getCustomField'")
-		logger.info("Looking for: " + str(Field))
-		return HardwareInfo['custom_fields'][Field]['value']
-
-
-
-############################################################Value Added functions##############################################################################
-	def getVariables(self,defaultResults):
+	def getVariables(self):
 		parser = argparse.ArgumentParser(description='Hello and welcome to the PyAsset.py script. They is pretty much just a ghetto api thing. Enjoy.   Here is a sample command:  ./PyAssets.py --function get --hardware "C02M631GFH05" // this can be any unique item identifier ')
 		parser.add_argument('-f','--function',type=str,help='REQUIRED: get, update, create, destroy, user')
 		parser.add_argument('-i','--hardware',type=str,help='REQUIRED: The key value you are searching on')
 		parser.add_argument('-u','--user',type=str,help='REQUIRED FOR CHECKOUT: example jak')
 		parser.add_argument('-t','--testing',type=str,help='enter the super secret test loop')
 		parser.add_argument('-n','--notes',type=str,help='Any notes you wish to append to checkin or checkout')
-		parser.add_argument('-q','--query',type=str,help='The fields you wish to return formatted "id,name,serial,..." ')
 		parser.add_argument('-s','--status',type=str,help='Status of the asset after checkin our checkout')
-
+		parser.add_argument('-q','--query',type=str,help='Return a specific result / results -- formatted "id,name,serial"')
 
 		#######Item Creation / Update Values#########
 		userVariables = parser.parse_args()
 		if not userVariables.function:
-			userVariables.function = 'get'
+			userVariables.function = raw_input("What function did you want to perform ('get','checkin','checkout'): ")
+			if userVariables.function == 'checkout':
+				userVariables.user = raw_input("Checkout to whom? ('jak'): ")
+		if not userVariables.hardware:
+			userVariables.hardware = response = raw_input("What asset are you going to perform it on? (provide a unique string i can search for): ")
+		if not userVariables.notes:
+			userVariables.notes = "Checkout by PyAsset Script"
 		if not userVariables.query:
-			userVariables.query = defaultResults
+			pass
 		else:
 			my_list = [str(item) for item in userVariables.query.split(',')]
-			userVariables.query = my_list
+			global defaultResults
+			defaultResults = my_list
 		return userVariables
 
 	def loggingSetup(self):
